62970305f09c136890e477bf2dc7c41ace086f62
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-02-02 23:16:04 +0530

    Tutorial for learning about While Loops

---
+++ Day - 9/Tut - 18.py
@@ -0,0 +1,29 @@
+# Learning about the While Loops.
+
+i = 0
+while(i<3):
+    print(i)
+    i = i + 1
+
+while (i<38):
+    i = int(input("Enter a number: "))
+    print(i)
+
+print("Done with While Loop")
+
+# A for loop is used when the number of iterations is known in advance or when looping through a sequence (like a list, string, or range).
+# Example use cases:
+# Looping through a list
+# Printing numbers from 1 to 10
+# Reading each character in a string
+
+# A while loop is used when the number of iterations is not known in advance and the loop continues as long as a condition is true.
+# You don’t know when the loop should stop
+# The loop depends on a condition changing during execution
+# Example use cases:
+# Waiting for correct user input
+# Running until a value reaches a target
+# Game or server loops
+
+# For loop → “Run this for each item / fixed count”
+# While loop → “Run this while a condition is true”
