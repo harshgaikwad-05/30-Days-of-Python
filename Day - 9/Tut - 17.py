f981b0fdd5381eb652dad3e24b537c6424a755f7
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-02-02 22:42:47 +0530

    Tutorial for learning about For Loops in Python

---
+++ Day - 9/Tut - 17.py
@@ -0,0 +1,17 @@
+# Learning about the For Loops in Python
+
+# Foe loops can iterate over a sequence of iterable objects in python.
+
+name = "Albert"
+for i in name:
+    print(i)
+
+list1 = ["orange", "red", "blue", "purple"]
+for color in list1:
+    print(color)
+
+for k in range(5):
+    print(k + 1)
+
+# The range function starts like : (start,stop,step)
+# We will use the for loops mostly for the upcoming programs.
