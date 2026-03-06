bf24ecd0df598da345e0b4b16985258c90526436
harshgaikwad-05 <harshgaikwad0517@gmail.com>
2026-03-06 21:56:31 +0530

    Tutorial for creating and activating a virtual environment

---
+++ Day - 21/Tut - 41.py
@@ -0,0 +1,11 @@
+# Virtual environment
+
+# Create a virtual environment 
+# python -m venv myenv
+
+# Activate he virtual environment (Windows)
+# myenv/Scripts/activate.bat 
+# myenv/Scripts/activate.ps1 (FOR THE POWERSHELL)
+
+# Activate he virtual environment (Linux/Macos)
+# source myenv/bin/activate
